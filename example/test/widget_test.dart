7e2c2f6a5ecd68a3d38d567e591798b9ea96deb9
behind47 <smalldragen@outlook.com>
2022-05-18 17:15:48 +0800

    首页代码放在example里

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:learnflutter/MyApp.dart';
 
-import 'package:learnflutter_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that platform version is retrieved.
     expect(
--- lib/ffi/native_add_page.dart
+++ lib/ffi/native_add_page.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:learnflutter/learnflutter.dart';
 
 class NativeAddPage extends StatefulWidget {
   @override
@@ -9,23 +11,47 @@ class NativeAddPage extends StatefulWidget {
 
 class NativeAddPageState extends State<NativeAddPage> {
 
-  late final DynamicLibrary nativeAddLib;
-
-  late final int Function(int x, int y) nativeAdd;
+  String _platformVersion = 'Unknown';
+  final _learnflutterPlugin = Learnflutter();
 
   @override
   void initState() {
-    nativeAddLib = DynamicLibrary.process();
-    nativeAdd = nativeAddLib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_add').asFunction();
     super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion =
+          await _learnflutterPlugin.getPlatformVersion() ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('使用ffi native add')),
-      body: Container(
-        child: Text('1+2=${nativeAdd(1,2)}'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on: $_platformVersion\n'),
+        ),
       ),
     );
   }
