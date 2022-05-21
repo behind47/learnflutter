05e5da10ca9bb86a84ff589ada807a058e9dd2d0
behind47 <smalldragen@outlook.com>
2022-05-21 22:09:38 +0800

    按照文档操作，..framework/Flutter/Debug下缺少FlutterPluginRegistrant.xcframework

---
+++ ..framework/Flutter/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<learnflutter/LearnflutterPlugin.h>)
+#import <learnflutter/LearnflutterPlugin.h>
+#else
+@import learnflutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LearnflutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"LearnflutterPlugin"]];
+}
+
+@end
