ab377cb391ecfdf576beab55900982580372f18b
behind47 <smalldragen@outlook.com>
2022-08-10 11:38:52 +0800

    删除错误代码

--- test/learnflutter_method_channel_test.dart
+++ test/learnflutter_method_channel_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:learnflutter/channel/learnflutter_method_channel.dart';
 
 void main() {
-  MethodChannelLearnflutter platform = MethodChannelLearnflutter();
   const MethodChannel channel = MethodChannel('learnflutter');
 
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
   });
 }
--- test/learnflutter_test.dart
+++ test/learnflutter_test.dart
@@ -1,28 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:learnflutter/channel/learnflutter_platform_interface.dart';
-import 'package:learnflutter/channel/learnflutter_method_channel.dart';
 import 'package:learnflutter/channel/platform_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockLearnflutterPlatform 
-    with MockPlatformInterfaceMixin
-    implements LearnflutterPlatform {
+    with MockPlatformInterfaceMixin {
 
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final LearnflutterPlatform initialPlatform = LearnflutterPlatform.instance;
-
-  test('$MethodChannelLearnflutter is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelLearnflutter>());
-  });
-
   test('getPlatformVersion', () async {
     PlatformChannel learnflutterPlugin = PlatformChannel();
-    MockLearnflutterPlatform fakePlatform = MockLearnflutterPlatform();
-    LearnflutterPlatform.instance = fakePlatform;
   
     expect(await learnflutterPlugin.getPlatformVersion(), '42');
   });
